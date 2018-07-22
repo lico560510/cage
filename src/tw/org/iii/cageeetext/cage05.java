9aebb22625b4561c31ebf4934888c176a0fcecad
user <user@iii-user-PC>
2018-07-22 14:42:28 +0800

    111

---
+++ src/tw/org/iii/cageeetext/cage05.java
@@ -0,0 +1,20 @@
+package tw.org.iii.cageeetext;
+
+public class cage05 {
+
+	public static void main(String[] args) {
+		char c1='a';
+		System.out.println(c1);
+		char c2='資';
+		System.out.println(c2);
+		char c3=65;
+		System.out.println(c3); //ASCII code
+		char c4 = '\u0041';
+		System.out.println(c4); //ASCII code
+		System.out.println(012);//0開頭視為8進位 0x開頭視為16進位
+		int a1=c1+4;
+		System.out.println(a1);//a是97
+		
+	}
+
+}
