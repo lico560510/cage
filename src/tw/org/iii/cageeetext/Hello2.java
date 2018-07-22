45be44fb22aaffad63a7e7c368102984304db3e5
user <user@iii-user-PC>
2018-07-22 12:26:33 +0800

    1111

--- src/tw/org/iii/cageeetext/Hello2.java
+++ src/tw/org/iii/cageeetext/Hello2.java
@@ -3,7 +3,7 @@ package tw.org.iii.cageeetext;
 public class Hello2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world2");
+		System.out.println("Hello world3");
 	}
 
 }
